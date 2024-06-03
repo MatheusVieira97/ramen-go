7b0e6e26969698e6ab5bf9cb53344f07f56e4481
Matheus Vieira <matheus.msv1997@gmail.com>
2024-06-03 19:25:09 -0300

    feat: adjust welcome banner component to desktop version

--- src/components/WelcomeBanner/WelcomeBanner.js
+++ src/components/WelcomeBanner/WelcomeBanner.js
@@ -1,5 +1,6 @@
 import Cover from '../Cover/Cover.js';
 import Header from '../Header/Header.js';
+import RoundedButton from '../RoundedButton/RoundedButton.js';
 import WelcomeBannerHtmlContent from './WelcomeBanner.html';
 
 import bannerRedPattern from '../../assets/images/pattern-red.svg';
@@ -7,20 +8,38 @@ import bannerRedPattern from '../../assets/images/pattern-red.svg';
 export default class WelcomeBanner extends HTMLElement {
   constructor() {
     super();
-    this.shadow = this.attachShadow({ mode: 'open' });
+    this.createElement();
     this.components = {
         Header,
-        Cover
+        Cover,
+        RoundedButton
     }
   }
 
-  connectedCallback() {
+  createElement() {
+    this.shadow = this.attachShadow({ mode: 'open' });
     const template = document.createElement('template');
     template.innerHTML = `${WelcomeBannerHtmlContent}`;
     const style = document.createElement('style');
     style.textContent = this.createStyles();
     this.shadow.appendChild(style);
     this.shadow.appendChild(template.content);
+  }
+  
+  connectedCallback() {
+    this.adjustButton()
+  }
+
+  adjustButton() {
+    const button = this.shadowRoot.querySelector('rmg-rounded-button').shadowRoot;
+    const container = button.querySelector('.button-container');
+    container.classList.add('centralize-left')
+    button.addEventListener('click', this.redirect)
+  }
+
+  redirect() {
+    debugger
+    document.getElementById('broths').scrollIntoView();
   }
 
   createStyles() {
@@ -54,6 +73,44 @@ export default class WelcomeBanner extends HTMLElement {
         text-align: center;
         width: 80%;
         font-family: 'Mplus-Medium'
+      }
+    }
+
+    @media(min-width: 1100px) {
+      .banner {
+        height: 100vh;
+      }
+
+      .banner__text {
+        align-items: baseline;
+        z-index: 1;
+        position: absolute;
+        top: 28%;
+        left: 19%;
+        padding-left: 7%;
+        overflow: hidden;
+
+        .tittle {
+          display: flex;
+          align-items: baseline;
+        }
+
+        h2 {
+          font-size: 4em;
+          writing-mode: vertical-lr
+        }
+
+        h3 {
+          font-size: 11em;
+        }
+
+        p {
+          width: 25%;
+          text-align: left;
+          padding-left: 3%;
+          font-size: 1.1em;
+          margin: 0;
+        }
       }
     }
     `
